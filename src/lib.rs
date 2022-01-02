dae0d22511237a04bb0bc5ebcff54ca6d666246e
Abiria <abiria.dev@gmail.com>
2022-01-02 17:00:34 +0900

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+pub enum Ty {
+    A(i32),
+    B(String),
+}
+
+impl From<i32> for Ty {
+    fn from(s: i32) -> Self {
+        Self::A(s)
+    }
+}
+
+impl From<String> for Ty {
+    fn from(s: String) -> Self {
+        Self::B(s)
+    }
+}
+
+#[allow(dead_code)]
+pub fn set(_: impl Into<Ty>) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        set(String::from("dslkdslk"));
+        set(4343);
+    }
+}
